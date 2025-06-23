0aa8837993773fa25d4b3bd10e3009ce15eaf8c9
BackDevWook <https://github.com/BackDevWook>
2025-06-23 20:14:12 +0900

    #2-1 Fixed

--- src/main/java/com/example/expertplugin/manager/CommandManager.java
+++ src/main/java/com/example/expertplugin/manager/CommandManager.java
@@ -1,6 +1,7 @@
 package com.example.expertplugin.manager;
 
 import com.example.expertplugin.ExpertPlugin;
+import com.example.expertplugin.command.QuestCommand;
 import com.example.expertplugin.command.SelectMode;
 import com.example.expertplugin.command.SetOp;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,6 +16,7 @@ public class CommandManager {
 
         Objects.requireNonNull(plugin.getCommand("mode")).setExecutor(new SelectMode()); // 게임 모드 변경
         Objects.requireNonNull(plugin.getCommand("op")).setExecutor(new SetOp()); // op 권한 설정
+        Objects.requireNonNull(plugin.getCommand("quest")).setExecutor(new QuestCommand()); // 퀘스트 조회
 
     }
 }
